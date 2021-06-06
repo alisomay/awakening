59d0943088a8678a0e65d20ad9f27a8a1dc6536c
Ali Somay <alisomay@runbox.com>
2021-06-06 23:58:28 +0200

    Fix chrome issue

--- src/App.js
+++ src/App.js
@@ -26,125 +26,124 @@ const isSafari =
 
 // let lastCreditIndex = 0;
 export const App = () => {
-  if ((isChrome || isFirefox) && !isSafari) {
-    const refToExperienceMain = React.createRef();
-    const refToStartButton = React.createRef();
-    const [init, setInit] = React.useState(false);
+  const refToExperienceMain = React.createRef();
+  const refToStartButton = React.createRef();
+  const [init, setInit] = React.useState(false);
 
-    const initExperience = (threeRef, videoRef, player) => {
-      const world = new World({
-        domElement: threeRef.current,
-        videoElement: videoRef.current,
-        player,
-      });
-      world.addObjects();
-      world.resize();
-      world.render();
-      world.setupListeners();
-      return world;
-    };
+  const initExperience = (threeRef, videoRef, player) => {
+    const world = new World({
+      domElement: threeRef.current,
+      videoElement: videoRef.current,
+      player,
+    });
+    world.addObjects();
+    world.resize();
+    world.render();
+    world.setupListeners();
+    return world;
+  };
 
-    useEffect(() => {
-      const startButton = refToStartButton.current;
-      const page2 = refToExperienceMain.current;
-      $(document).ready(() => {
-        $('#experience-container').css('display', 'none');
-      });
+  useEffect(() => {
+    const startButton = refToStartButton.current;
+    const page2 = refToExperienceMain.current;
+    $(document).ready(() => {
+      $('#experience-container').css('display', 'none');
+    });
 
-      $(startButton).on('click', () => {
-        $('#experience-container').css('display', 'flex');
-        $('html, body').animate(
-          {
-            scrollTop: $(page2).offset().top,
-          },
-          1600,
-          () => {
-            setInit(true);
-            $('html, body')
-              .css('overflow-x', 'hidden')
-              .css('overflow-y', 'hidden');
+    $(startButton).on('click', () => {
+      $('#experience-container').css('display', 'flex');
+      $('html, body').animate(
+        {
+          scrollTop: $(page2).offset().top,
+        },
+        1600,
+        () => {
+          setInit(true);
+          $('html, body')
+            .css('overflow-x', 'hidden')
+            .css('overflow-y', 'hidden');
 
-            $('#loading').fadeIn();
-            // Scroll Complete
-            // Start exp
-            // If we want to change url it is here
-          },
-        );
-      });
+          $('#loading').fadeIn();
+          // Scroll Complete
+          // Start exp
+          // If we want to change url it is here
+        },
+      );
+    });
 
-      return () => {};
-    }, []);
-    useEffect(() => {
-      console.log(
-        '%cRoz Yuen - Awakening',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:4rem;font-weight:bold',
-      );
-      console.log(
-        '%cPerformer/Composer/Producer',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cRoz Yuen',
-        'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cMix Engineer (Original Track):',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cAndrew Hockey',
-        'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cMix Engineer (Binaural Version):',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cRoz Yuen',
-        'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cMastering',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cPhilip Röder (Copilco Productions)',
-        'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cArt Direction',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cRoz Yuen and Başak Ünal',
-        'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cDigital Artist',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cBaşak Ünal',
-        'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cDeveloper/Creative Technologist',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cAli Somay',
-        'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cThis project was funded by',
-        'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-      console.log(
-        '%cMusikfonds e.V.',
-        'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
-      );
-    }, []);
-  }
+    return () => {};
+  }, []);
+  useEffect(() => {
+    console.log(
+      '%cRoz Yuen - Awakening',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:4rem;font-weight:bold',
+    );
+    console.log(
+      '%cPerformer/Composer/Producer',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cRoz Yuen',
+      'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cMix Engineer (Original Track):',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cAndrew Hockey',
+      'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cMix Engineer (Binaural Version):',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cRoz Yuen',
+      'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cMastering',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cPhilip Röder (Copilco Productions)',
+      'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cArt Direction',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cRoz Yuen and Başak Ünal',
+      'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cDigital Artist',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cBaşak Ünal',
+      'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cDeveloper/Creative Technologist',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cAli Somay',
+      'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cThis project was funded by',
+      'color:#E65E5E;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+    console.log(
+      '%cMusikfonds e.V.',
+      'color:#fff;font-family:GrandSlang-Roman;font-size:1rem;font-weight:bold',
+    );
+  }, []);
+
   if ((isChrome || isFirefox) && !isSafari) {
     return (
       <div className="app">
